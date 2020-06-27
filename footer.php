3da71cb30c629b0675e71f942e800582aa995e26
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-06-27 14:57:46 +0900

    firstpush

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="l-footer">
+        <ul class="l-sns_group">
+            <li><a href=""><i class="lab la-twitter la-2x"></i></a></li>
+            <li><a href=""><i class="lab la-instagram la-2x"></i></a></li>
+            <li><a href=""><i class="lab la-youtube la-2x"></i></a></li>
+        </ul>
+        <address class="l-copyright">&copy;FLAGLOD All Rights  Reserved</address>
+    </footer>
+</body>
+</html>
