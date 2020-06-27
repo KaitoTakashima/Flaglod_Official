d51d6f8a7b4342a2eaaafadeee45101476a5edd5
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-06-27 14:58:56 +0900

    firstpush

---
+++ archive-profile.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+    <main class="biography_main_style">
+        <section class="p-biography">
+            <h2 class="c-biography_heading c-profile_bg">略歴</h2>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                    文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                    文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+            </p>
+            </div>
+        </section>
+    </main>
+
+  
+<!-- footer.phpを読み込む -->
+<?php get_footer();?>
