61a5b2cddd63344c4d17082b743cde355c5227f5
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-07-05 09:52:40 +0900

    タイトルのタグを<h1>に

---
+++ archive-biography.php
@@ -0,0 +1,97 @@
+<?php get_header(); ?>
+
+    <section class="p-biography">
+    <h1 class="c-biography_heading c-biography_bg">略歴</h1>
+        <div class="c-bio_bg"></div>
+        <div class="p-band_profile">
+            <p>文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。</p>
+            <br>
+            <p>文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。</p>
+            <p>文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。</p>
+        </div>
+
+        <section class="p-membar_profile">
+            <div class="p-member_infomation">
+                <div class="c-member_picture c-kaisei_picture c-fadein"></div>
+                <div class="p-profile p-left_profile c-fadein_left">
+                    <p class="c-member_responsible">Vocal & Guiter</p>
+                    <p class="c-member_name">名前</p>
+                        <ul class="p-detail_info">
+                            <li class="c-member_bath">1234年5月6日生</li>
+                            <li class="c-member_sign">〇〇座</li>
+                            <li class="c-member_bloodtype">〇型</li>
+                        </ul>
+                </div>
+            </div>
+
+            <div class="p-member_infomation">
+                <div class="c-member_picture c-sengan_picture c-fadein"></div>
+                <div class="p-profile p-right_profile c-fadein_right">
+                    <p class="c-member_responsible">Guiter</p>
+                    <p class="c-member_name">名前</p>
+                        <ul class="p-detail_info">
+                            <li class="c-member_bath">1234年5月6日生</li>
+                            <li class="c-member_sign">〇〇座</li>
+                            <li class="c-member_bloodtype">〇型</li>
+                        </ul>
+                </div>
+            </div>
+
+            <div class="p-member_infomation">
+                <div class="c-member_picture c-isema_picture c-fadein"></div>
+                <div class="p-profile p-left_profile c-fadein_left">
+                    <p class="c-member_responsible">Bass</p>
+                    <p class="c-member_name">名前</p>
+                        <ul class="p-detail_info">
+                            <li class="c-member_bath">1234年5月6日生</li>
+                            <li class="c-member_sign">〇〇座</li>
+                            <li class="c-member_bloodtype">〇型</li>
+                        </ul>
+                </div>
+            </div>
+
+            <div class="p-member_infomation">
+                <div class="c-member_picture c-kaito_picture c-fadein"></div>
+                <div class="p-profile p-right_profile c-fadein_right">
+                    <p class="c-member_responsible">Drum</p>
+                    <p class="c-member_name">名前</p>
+                        <ul class="p-detail_info">
+                            <li class="c-member_bath">1234年5月6日生</li>
+                            <li class="c-member_sign">〇〇座</li>
+                            <li class="c-member_bloodtype">〇型</li>
+                        </ul>
+                </div>
+            </div>
+        </sction>
+            <!-- <h2 class="c-biography_heading c-profile_bg">略歴</h2>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                    文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                    文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+                </p>
+            </div>
+
+            <div class="p-biography_content">
+                <p class="c-biography_day">2020.06.19</p>
+                <p class="c-biography_text">文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。文章が入ります。
+            </p>
+            </div> -->
+    </section>
+
+
+<!-- footer.phpを読み込む -->
+<?php get_footer();?>
