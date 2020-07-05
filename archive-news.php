c49f7d7ab0a99714225b56a799ff6d78ad975079
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-07-05 09:53:10 +0900

    タイトルのタグを<h1>に

--- archive-news.php
+++ archive-news.php
@@ -1,36 +1,47 @@
 <?php get_header(); ?>
    <main>
         <section class="p-News">
-            <h2 class="c-news_heading c-news_bg">お知らせ</h2>
-        <?php if(have_posts()) : ?>
-            <?php while(have_posts()) : the_post(); ?>
-            <div <?php post_class(); ?>>
-            <!-- タイトルにリンクを付けて表示 -->
-                <ul>
-                        <li class="news_detail">
-                            <p class="c-news_day">
-                                <?php the_time('Y/m/d');?>
-                            </p>
-                        </li>
-                        <li class="news_content">
-                            <a href="<?php the_permalink(); ?>">
-                                <p class="c-news_title">
-                                    <?php the_title();?>
+            <h1 class="c-news_heading c-news_bg">お知らせ</h1>
+            <?php if(have_posts()) : ?>
+                <?php while(have_posts()) : the_post(); ?>
+                <div <?php post_class(); ?>>
+                <!-- タイトルにリンクを付けて表示 -->
+                    <ul>
+                            <li class="news_detail">
+                                <p class="c-news_day">
+                                    <?php the_time('Y/m/d');?>
                                 </p>
-                            </a>
-                        </li>
-                </ul>
-            </div>
-            <?php endwhile; ?>
-         <?php endif; ?>
+                            </li>
+                            <li class="news_content">
+                                <a href="<?php the_permalink(); ?>">
+                                    <p class="c-news_title">
+                                        <?php the_title();?>
+                                    </p>
+                                </a>
+                            </li>
+                    </ul>
+                </div>
+                <?php endwhile; ?>
 
-            </section>
-        <section class="c-pagination">
-            <ul class="c-pagination_btn">
-                <li class="c-back_btn"><a href="">前へ</a></li>
-                <li class="c-next_btn"><a href="">次へ</a></li>
-            </ul>
+            <div class="c-mobile_pagination">
+                <?php
+                    the_posts_navigation( array(
+                        'prev_text'     => '次へ',
+                        'next_text'     => '前へ '
+                    ) );
+                ?>
+            </div>
+                <div class="c-pc_pagination">
+                    <?php if(function_exists('wp_pagenavi')) :
+                        wp_pagenavi(); //wp_pagenavi()の呼び出し
+                    endif;
+                    ?>
+                </div>
+
+            <?php endif; ?>
+
         </section>
+
     </main>
 
 
